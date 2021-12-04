a37fdfdefa37395da18427726257ee87ad59485b
Alexandre Le Bacquer <alexandre.lbac@gmail.com>
2021-12-04 17:28:05 +0100

    BatimentController
    UwU

---
+++ GameJamCare2021/Assets/Scripts/BatimentController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatimentController : MonoBehaviour
+{
+    public GameObject car;
+    [SerializeField]
+    private float rayDistance = 0;
+    [SerializeField]
+    private LayerMask layers = default;
+    private bool collide;
+
+    RaycastHit hit;
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * rayDistance, Color.red);
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, rayDistance, layers))
+        {
+            collide = true;
+        }
+        else
+        {
+            collide = false;
+        }
+    }
+
+    private void Collide()
+    {
+        if (collide == true)
+        {
+            string selection = hit.transform.gameObject.name;
+
+            if (selection == car.name)
+            {
+
+            }
+        }
+    }
+}
+
