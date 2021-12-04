1007a094393f05eacd92c6c7ee2155e862cd9cde
Enzo-Edele <78355035+Enzo-Edele@users.noreply.github.com>
2021-12-04 14:29:11 +0100

    Script Base

---
+++ GameJamCare2021/Assets/Scripts/VehicleCenterManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleCenterManager : MonoBehaviour
+{
+    [SerializeField]GameObject CarPrefab;
+    List<GameObject> vehicleList;
+
+    void Start()
+    {
+        vehicleList = new List<GameObject>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void InstanceCar()
+    {
+        GameObject car = Instantiate(CarPrefab, transform.position, Quaternion.identity);
+
+    }
+}
