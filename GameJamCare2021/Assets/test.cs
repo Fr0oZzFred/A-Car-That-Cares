23dd70afad49eaf6e2ad5e48b33704efa6b4d150
Fr0oZzFred <78353908+Fr0oZzFred@users.noreply.github.com>
2021-12-04 20:35:24 +0100

    TransitionMaison

---
+++ GameJamCare2021/Assets/test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public int indexZ;
+    public List<GameObject> goZ;
+
+    private void Start() {
+        goZ = new List<GameObject>();
+        int tutu = goZ[0].transform.childCount;
+        for(int x = 0; x < goZ.Count; x++) {
+            for(int z = 0; z < goZ[x].transform.childCount; z++) {
+                //goz[i,z] = gameobbject.Find();
+            }
+        }
+    }
+}
