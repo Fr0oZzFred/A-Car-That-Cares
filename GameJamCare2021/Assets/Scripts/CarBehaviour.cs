200d7bfcf4ac5bd8c96211409eb297f9d0da9c4f
Nibofar <quentin.bouletbenac@gmail.com>
2021-12-04 19:39:45 +0100

    third share

--- GameJamCare2021/Assets/Scripts/CarBehaviour.cs
+++ GameJamCare2021/Assets/Scripts/CarBehaviour.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 public class CarBehaviour : MonoBehaviour{
     public MapPathFinding map;
-    public static CarBehaviour globalCar;
-    public int startPosX, startPosY;
+    public static GameObject globalCar;
+    public Vector2Int startPos;
     Cell currentCell;
     List<Cell> goToList = new List<Cell>();
-    void Awake(){
-        globalCar = this;
-        currentCell = map.map[startPosX, startPosY];
+    void Start(){
+        globalCar = this.gameObject;
+        currentCell = map.map[startPos.x, startPos.y];
         currentCell.car = true;
     }
     void Update(){
-        
+        if (goToList.Count > 0){
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(goToList[0].transform.position.x, 1, goToList[0].transform.position.z), Time.deltaTime * 2);
+            if (transform.position == goToList[0].transform.position){
+                currentCell = goToList[0];
+                goToList.RemoveAt(0);
+            }
+        }
     }
     public void GoTo(Cell target){
         Cell start = currentCell;
         if (goToList.Count > 0) start = goToList[goToList.Count - 1];
-
+        goToList.AddRange(map.PathFind(start, target));
     }
 }
--- GameJamCare2021/Assets/Scripts/Cell.cs
+++ GameJamCare2021/Assets/Scripts/Cell.cs
@@ -5,10 +5,14 @@ public class Cell : MonoBehaviour{
     public GameObject trail;
     public bool house;
     [HideInInspector] public bool car;
+    public static GameObject cell;
     [System.NonSerialized] public List<Cell> neighbors;
     [System.NonSerialized] public Cell parent;
     [System.NonSerialized] public bool visited;
     [System.NonSerialized] public Node<Cell> node;
+    private void Start(){
+        cell = this.gameObject;
+    }
     void Update(){
         if (car) trail.SetActive(false);
         if (trail.activeInHierarchy == false) car = false;
