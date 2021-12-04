88e4bdb914a34fb190edeaf69e13b998c3808f59
Frédéric Deiber <frederic.deiber@gmail.com>
2021-12-04 23:56:00 +0100

    Road

--- GameJamCare2021/Assets/test.cs
+++ GameJamCare2021/Assets/test.cs
@@ -3,16 +3,18 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
-    public int indexZ;
+    int indexZ;
+    //Debug.Log(transform.childCount); childCount ne prend en compte les enfants d'un enfant
     public List<GameObject> goZ;
-
+    int[] tutu;
     private void Start() {
-        goZ = new List<GameObject>();
+        /*goZ = new List<GameObject>();
         int tutu = goZ[0].transform.childCount;
         for(int x = 0; x < goZ.Count; x++) {
             for(int z = 0; z < goZ[x].transform.childCount; z++) {
-                //goz[i,z] = gameobbject.Find();
+                //goz[i,z] = gameobbject.Find($"Cube{Z}"); Soucis avec les noms, nomeclature chiant?
             }
-        }
+        }*/
+        //ToArray() est suffisant pour Quentin? ou fonctione spécial pour lui?
     }
 }
