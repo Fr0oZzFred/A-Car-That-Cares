e173f35522dafee2798a6a54efc30c53bdbc6dcc
Fr0oZzFred <78353908+Fr0oZzFred@users.noreply.github.com>
2021-12-05 17:13:43 +0100

    fbx

--- GameJamCare2021/Assets/Scripts/GetGrid.cs
+++ GameJamCare2021/Assets/Scripts/GetGrid.cs
@@ -8,6 +8,7 @@ public class GetGrid : MonoBehaviour {
     int indexZMax = 0; //Récup la ranger la plus grande
     GameObject[,] grid;
     private void Start() {
+        //récup GO
         goZ = new List<GameObject>();
         for (int x = 0; x < goX.Count; x++) {
             for (int z = 1; z <= goX[x].transform.childCount; z++) {
@@ -16,23 +17,18 @@ public class GetGrid : MonoBehaviour {
             if (goX[x].transform.childCount > indexZMax) indexZMax = goX[x].transform.childCount;
         }
 
-        //Converttion en array[,] attention à changer le comblage des cases vides
+        //Conversion en array[,] attention à changer le comblage des cases vides
         grid = new GameObject[goX.Count, indexZMax + 1];
         for (int x = 0; x < grid.GetLength(0); x++) {
             grid[x, 0] = goX[x];
             for (int z = 0; z < grid.GetLength(1) - 1; z++) {
                 if (goX[x].transform.childCount > z && goZ.Count > 0) {
-                    grid[x, z + 1] = goZ[z];
+                    grid[x, z + 1] = goZ[z]; //Donner les GO actifs
                 } else {
                     grid[x, z + 1] = new GameObject(); // à changer
                 }
             }
             goZ.RemoveRange(0, goX[x].transform.childCount);
-        }
-        for (int x = 0; x < grid.GetLength(0); x++) {
-            for (int z = 0; z < grid.GetLength(1); z++) {
-                Debug.Log(grid[x, z], grid[x, z]);
-            }
         }
     }
 }
