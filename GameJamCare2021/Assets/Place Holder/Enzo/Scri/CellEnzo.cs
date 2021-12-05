afa91ad568d66c591772659fad7dcab64bee5a94
Enzo-Edele <78355035+Enzo-Edele@users.noreply.github.com>
2021-12-05 13:04:46 +0100

    Mimic PathFinding

---
+++ GameJamCare2021/Assets/Place Holder/Enzo/Scri/CellEnzo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellEnzo : MonoBehaviour
+{
+    public GameObject wallObject;
+    public bool IsWall { get; set; }
+    [System.NonSerialized] public bool visited;
+    [System.NonSerialized] public List<CellEnzo> neighbors;
+    [System.NonSerialized] public CellEnzo parent;
+    [System.NonSerialized] public NodeEnzo<CellEnzo> node;
+
+    public void SetWall(bool wall)
+    {
+        IsWall = wall;
+        wallObject.SetActive(wall);
+    }
+
+    public void SetMaterial(Material mat)
+    {
+        GetComponent<MeshRenderer>().material = mat;
+    }
+
+    private void OnMouseDown()
+    {
+        CharacterEnzo.globalCharacter.GoTo(this);
+    }
+
+}
---
+++ GameJamCare2021/Assets/Place Holder/Enzo/Scri/CharacterEnzo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterEnzo : MonoBehaviour
+{
+    public GridPathFindingEnzo grid;
+    public static CharacterEnzo globalCharacter; //pas faire ca marche mais c po bien
+    CellEnzo currentCell;
+    [SerializeField]List<CellEnzo> goToList;
+
+
+    void Start()
+    {
+        globalCharacter = this;
+        currentCell = grid.grid[0, 0];
+        goToList = new List<CellEnzo>();
+    }
+
+    public void GoTo(CellEnzo target)
+    {
+        CellEnzo start = currentCell;
+        if (goToList.Count > 0) start = goToList[goToList.Count - 1];
+        goToList.AddRange(grid.PathFind(start, target));
+    }
+
+    void Update()
+    {
+        if (goToList.Count > 0)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                goToList[0].transform.position,
+                Time.deltaTime * 5);
+            if (transform.position == goToList[0].transform.position)
+            {
+                currentCell = goToList[0];
+                goToList.RemoveAt(0);
+            }
+        }
+    }
+}
